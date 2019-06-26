6e6f3ab8fa51561b86bfddfcd915929e62d5a951
Ud101298 <vddeshya10d@gmail.com>
2019-06-26 15:19:55 +0530

    School Solution

---
+++ link.js
@@ -0,0 +1,21 @@
+function Password()
+{
+	document.getElementById('login').style.display="none";
+	document.getElementById('login-box').style.display="none";
+	document.getElementById('forgot').style.display="block";
+	document.getElementById('dash').style.display="none";
+}
+function index()
+{
+	document.getElementById('login').style.display="none";
+	document.getElementById('login-box').style.display="block";
+	document.getElementById('forgot').style.display="none";
+	document.getElementById('dash').style.display="none";
+}
+function dash()
+{
+	document.getElementById('login').style.display="none";
+	document.getElementById('login-box').style.display="none";
+	document.getElementById('forgot').style.display="none";
+	document.getElementById('dash').style.display="block";
+}
